95eb179ae3e57020a21ad306988e5d93ed186c13
andfield <siddharththakur76@gmail.com>
2021-05-17 15:26:20 +1200

    Clean slate

--- client/src/services/emailService.js
+++
@@ -1,9 +0,0 @@
-import api from '@/services/api.js'
-export default {
-
-    staytuned(data) {
-        return api().post('/emailStayTuned', data)
-    }
-
-
-}
--- server/src/models/followers.js
+++
@@ -1,8 +0,0 @@
-
-module.exports=(sequelize, DataTypes) => {
-    const following = sequelize.define('following', {
-        userId: DataTypes.STRING,
-        followerId: DataTypes.STRING
-    })
-    return following
-}
